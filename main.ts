0ea8fa48853c9599eb7f8fdc35ee912a0ad7abe1
Isaac Zhang <52984823+Lotuus@users.noreply.github.com>
2020-09-25 12:30:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onGesture(Gesture.Shake, function () {
+    makerController.player1.reset()
+    makerController.player1.press(ArcadeButton.A)
+    light.showRing(
+    `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
+    )
+    makerController.player1.reset()
+    control.waitMicros(2000000)
+})
+forever(function () {
+    if (input.buttonA.isPressed()) {
+        makerController.player1.reset()
+        light.showRing(
+        "red red red red red red red red red red"
+        )
+        makerController.player1.setButton(ArcadeButton.Left, true)
+    } else if (input.buttonB.isPressed()) {
+        makerController.player1.reset()
+        light.showRing(
+        `blue blue blue blue blue blue blue blue blue blue`
+        )
+        makerController.player1.setButton(ArcadeButton.Right, true)
+    } else {
+        makerController.player1.reset()
+        light.showRing(
+        `black black black black black black black black black black`
+        )
+    }
+})

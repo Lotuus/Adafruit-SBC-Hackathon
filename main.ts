50d920803a84b807ee152b2645cdd5e5e9a71895
Isaac Zhang <52984823+Lotuus@users.noreply.github.com>
2020-09-25 14:01:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,33 +2,22 @@ input.onGesture(Gesture.Shake, function () {
     makerController.player1.setButton(ArcadeButton.Left, false)
     makerController.player1.setButton(ArcadeButton.Right, false)
     makerController.player1.press(ArcadeButton.A)
-    light.showRing(
-    `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
-    )
+    light.onboardStrip().showAnimationFrame(light.sparkleAnimation)
     makerController.player1.setButton(ArcadeButton.A, false)
-    light.showRing(
-    `black black black black black black black black black black`
-    )
-    control.waitMicros(500000)
+    makerController.player1.setButton(ArcadeButton.Left, false)
+    makerController.player1.setButton(ArcadeButton.Right, false)
 })
 forever(function () {
     if (input.buttonA.isPressed()) {
-        makerController.player1.reset()
-        light.showRing(
-        "red red red red red red red red red red"
-        )
+        makerController.player1.setButton(ArcadeButton.Left, false)
+        makerController.player1.setButton(ArcadeButton.Right, false)
         makerController.player1.setButton(ArcadeButton.Left, true)
     } else if (input.buttonB.isPressed()) {
-        makerController.player1.reset()
-        light.showRing(
-        `blue blue blue blue blue blue blue blue blue blue`
-        )
+        makerController.player1.setButton(ArcadeButton.Left, false)
+        makerController.player1.setButton(ArcadeButton.Right, false)
         makerController.player1.setButton(ArcadeButton.Right, true)
     } else {
         makerController.player1.setButton(ArcadeButton.Left, false)
         makerController.player1.setButton(ArcadeButton.Right, false)
-        light.showRing(
-        `black black black black black black black black black black`
-        )
     }
 })
